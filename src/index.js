65e492e478bfbdec9d416f11b9606d570eb97096
Damyan Peykov <dppeykov@gmail.com>
2019-09-09 10:51:18 +0200

    Basic routing with a switch

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import App from "./components/App";
+import Users from "./components/Users";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+const routing = (
+  <Router>
+    <div>
+      {
+        // The navigation can be in different component - for bigger apps
+      }
+
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      {
+        // The switch allows to render only the matches - the rest is falling back to NotFound
+      }
+
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(routing, document.getElementById("root"));
